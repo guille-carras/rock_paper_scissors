45693cf13322516c0cad890fc07223556253862e
Guille <guillecarrasquilla@gmail.com>
2024-04-06 10:16:16 -0500

    create the playRound and playGame functions
    playRound to compare the options choosen
    
    playGame to play the game 5 times and define
    whether the player win or lose

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let wins = 0;
+let draws = 0;
+let loses = 0;
 
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3) + 1;
@@ -14,4 +17,74 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    let result;
+    switch(playerSelection) {
+        case 'rock': {
+            if (computerSelection === 'rock') {
+                draws += 1;
+                result = `It's draw! ${playerSelection} vs ${computerSelection}.`;
+                return result;
+            } else if (computerSelection === 'paper') {
+                wins += 1;
+                result = `You wins! ${playerSelection} beats ${computerSelection}.`;
+                return result;
+            } else {
+                loses += 1;
+                result = `You loses! ${computerSelection} beats ${playerSelection}.`;
+                return result;
+            }
+        }
+        case 'paper': {
+            if (computerSelection === 'rock') {
+                wins += 1;
+                result = `You wins! ${playerSelection} beats ${computerSelection}.`;
+                return result;
+            } else if (computerSelection === 'paper') {
+                draws += 1;
+                result = `It's draw! ${playerSelection} vs ${computerSelection}.`;
+                return result;
+            } else {
+                loses += 1;
+                result = `You loses! ${computerSelection} beats ${playerSelection}.`;
+                return result;
+            }
+        }
+        case 'scissors': {
+            if (computerSelection === 'rock') {
+                loses += 1;
+                result = `You loses! ${computerSelection} beats ${playerSelection}.`;
+                return result;
+            } else if (computerSelection === 'paper') {
+                wins += 1;
+                result = `You wins! ${playerSelection} beats ${computerSelection}.`;
+                return result;
+            } else {
+                draws += 1;
+                result = `It's draw! ${playerSelection} vs ${computerSelection}.`;
+                return result;
+            }
+        }
+    }
+}
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        const optionPlayer = prompt('Choose an option between Rock, Paper or Scissors').toLowerCase();
+        const optionCumputer = getComputerChoice();
+        const result = playRound(optionPlayer, optionCumputer);
+        console.log(result);
+    }
+    if(wins > loses) {
+        console.log(`You are the winner! with ${wins} wins.`);
+    } else if (wins < loses) {
+        console.log(`You are the loser with ${loses} defeats, try again! `);
+    } else {
+        console.log(`It's a draw, your wins ${wins} vs the wins of the oponent ${loses}.`);
+    }
+}
+
+
+
+
+
